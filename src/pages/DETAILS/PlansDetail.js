e8487d9569160eda9b1d647833ba27a640bf0b58
lilymareli <mervekucukofficial@gmail.com>
2020-07-29 01:15:35 +0300

    plansdetail page

--- src/pages/DETAILS/PlansDetail.js
+++ src/pages/DETAILS/PlansDetail.js
@@ -1,12 +1,20 @@
-import React, {useState} from 'react'
-import { SafeAreaView, View, TouchableOpacity, Text, FlatList, Dimensions, Modal, Pressable } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView, View, TouchableOpacity, Text, Image, TextInput, FlatList, Dimensions, Pressable } from 'react-native'
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import Modal from 'react-native-modal';
 
 
 const PlansDetail = () => {
     const [modalVisible, setModalVisible] = useState(false);
-    const [plan, setPlan] = useState("")
-    const [date, setDate] = useState("")
+    const [planTitle, setPlanTitle] = useState("")
+    const [planDetail, setPlanDetail] = useState("")
+    const [planDate, setPlanDate] = useState("")
+    const [allPlans, setAllPlans] = useState([])
+    const deviceWidth = Dimensions.get("window").width;
+    const deviceHeight = Dimensions.get("window").height;
+    const vacation = { key: 'vacation', color: 'red', selectedDotColor: 'blue' };
+    const massage = { key: 'massage', color: 'blue', selectedDotColor: 'blue' };
+    const workout = { key: 'workout', color: 'green' };
 
     /*
        1. OPTIONAL İNSANLARI EKLEME
@@ -16,9 +24,10 @@ const PlansDetail = () => {
        5. OPTIONAL TARİH-SAAT EKLEME
        
    */
-    const vacation = { key: 'vacation', color: 'red', selectedDotColor: 'blue' };
-    const massage = { key: 'massage', color: 'blue', selectedDotColor: 'blue' };
-    const workout = { key: 'workout', color: 'green' };
+
+   function addPlan () {
+       //push details to allplans
+   }
 
     return (
         <SafeAreaView>
@@ -27,37 +36,47 @@ const PlansDetail = () => {
                     <Text>Plans</Text>
                 </TouchableOpacity>
                 <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert("Modal has been closed.");
-        }}
-      >
-           <View >
-          <View >
-            <Text>Hello World!</Text>
+                    isVisible={modalVisible}
+                    deviceWidth={deviceWidth}
+                    deviceHeight={deviceHeight}
+                    onSwipeComplete={() => setModalVisible(false)}
+                    swipeDirection="down"
+                    style={{ margin: 15, marginTop: 300, }}
+                >
+                    <View style={{ flex: 1, backgroundColor: '#f2f2f2', borderRadius: 20, }}>
+                        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 
-            <Pressable
-              style={{ backgroundColor: "#2196F3" }}
-              onPress={() => {
-                setModalVisible(!modalVisible);
-              }}
-            >
-              <Text>Hide Modal</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
+                            <Text
+                                style={{ fontSize: 15 }}
+                            >Plan:</Text>
+                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                <Image source={require('../../assets/checklist.png')} />
+                                <TextInput
+                                    autoCapitalize="none"
+                                    placeholder="Plan"
+                                    style={{ margin: 0, padding: 3 }}
+                                />
+                            </View>
+                                <Pressable
+                                    style={{
+                                        backgroundColor: "#2196F3",
+                                    }}
+                                    onPress={() => addPlan()} >
+                                    <Text>Ekle</Text>
+                                </Pressable>
+                        </View>
+                    </View>
+                </Modal>
+
                 <CalendarList
                     // Callback which gets executed when visible months change in scroll view. Default = undefined
                     onVisibleMonthsChange={(months) => { console.log('now these months are visible', months); }}
-                  
-                    // Enable or disable scrolling of calendar list
+
                     scrollEnabled={true}
-                    // Enable or disable vertical scroll indicator. Default = false
-                    showScrollIndicator={true}
-                    onDayPress={(day) => { console.log('selected day', day); setModalVisible(!modalVisible) }}
+                    
+                    //  selected day {"dateString": "2020-08-28", "day": 28, "month": 8, 
+                    //"timestamp": 1598572800000, "year": 2020}
+                    onDayPress={(day) => { setPlanDate(day), console.log('selected day', planDate); setModalVisible(!modalVisible) }}
                     onMonthChange={(month) => { console.log('month changed', month) }}
 
                     markedDates={{
@@ -66,12 +85,12 @@ const PlansDetail = () => {
                     }}
                     markingType={'multi-dot'}
 
-                   
+
                     style={{
                         borderWidth: 1,
                         borderColor: 'gray',
                         height: 350
-                      }}
+                    }}
                     firstDay={1}
                 />
             </View>

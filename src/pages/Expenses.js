5446ea17220d00ee103ab40d6ac7244368dbc3f6
girisimci <saitisik63@gmail.com>
2020-08-15 13:53:24 +0300

    expenses aktif :)

--- src/pages/Expenses.js
+++ src/pages/Expenses.js
@@ -1,27 +1,43 @@
-import React from 'react'
-import { SafeAreaView, View, TouchableOpacity, Text, FlatList, Dimensions } from 'react-native'
-import addPara from '../components'
-import itemPara from '../components'
-const Expenses = () => {
-
-    /*
-        GÜN GÜN TOPLAM HARCAMA LİSTESİ
-        GRAFİK İLE GÖSTEREBİLİRİZ
-        TIKLAYINCA DETAY SAYFASINA GİDER
+import React, { useState } from 'react';
+import { SafeAreaView, View,StyleSheet, 
+    TouchableOpacity, Text,
+     FlatList, Dimensions } from 'react-native';
 
 
-    */
-
-
+ const Expenses=()=> {
+    const [todos,setTodos]= useState([
+        {text: 'burada çok harcadın', key: '1'},
+        {text: 'şurada az harcadın', key:'2'},
+        {text: 'paran bitti', key:'3'}
+    ]);
     return(
-        <SafeAreaView>
-            <View>
-                <TouchableOpacity>
-                           
-                </TouchableOpacity>
+        <View style={styles.container}>
+            {/**header */}
+            <View style={styles.content}>
+                {/**to form */}
+                <View style={styles.list}>
+                    <FlatList
+                    data={todos}
+                    renderItem={({item})=>(
+                    <Text> {item.text} </Text>
+                    )}
+                        />
+                </View>
             </View>
-        </SafeAreaView>
-    )
+        </View>
+    );
 }
+export{Expenses}
 
-export {Expenses}
+const styles= StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#fff',
+    },
+    content:{
+        padding:40,
+    },
+    list:{
+        
+    }
+})
